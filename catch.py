eac8e27d087889a075bd8da4d3b075428e3728e6
WEI-CHE <wery6129@gmail.com>
2026-06-25 15:29:04 +0800

    Add serial communication functionality and initial data logging

---
+++ catch.py
@@ -0,0 +1,44 @@
+import serial
+import time
+PACKET_SIZE = 35
+HEADER = b'\xAA\x01'
+
+ser = serial.Serial("COM3", 115200, timeout=1)
+
+def read_one_packet(ser):
+    ser.reset_input_buffer()
+
+    # 不斷往前找 header
+    while True:
+        b = ser.read(1)
+        if not b:
+            return None  # timeout
+
+        if b == HEADER[:1]:   # 先遇到 0xAB
+            b2 = ser.read(1)
+            if not b2:
+                return None
+
+            if b2 == HEADER[1:]:   # 接著是 0xAA
+                # 已經抓到 header 的前 2 bytes
+                rest = ser.read(PACKET_SIZE - 2)
+                if len(rest) != PACKET_SIZE - 2:
+                    return None
+                return HEADER + rest
+
+def read_serial_packet(ser):
+    p = ser.read(PACKET_SIZE)
+    if not p:
+        return None
+    return p
+
+packet_received = 0;
+while packet_received < 17:
+    packet = read_one_packet(ser)
+    if packet:
+        print(packet.hex(" "))
+
+        with open("press_img.txt", "a", encoding="utf-8") as f:
+            f.write(str(packet_received) + ": " + packet.hex(" ") + '\n')
+        packet_received += 1
+        time.sleep(3)

dd25ccf964c97a238317adf7257601adcd64dc03
Amber <eric100060@gmail.com>
2026-06-25 17:06:06 +0800

    Refactor timeCatch.py to initialize dataArr and save received data to a timestamped file

--- timeCatch.py
+++ timeCatch.py
@@ -1,5 +1,6 @@
 import serial
 import time
+from datetime import datetime
 
 PACKET_SIZE = 35
 TIMESET = 5 #5s
@@ -8,10 +9,14 @@ ser = serial.Serial('COM4', 115200, timeout=1)
 count = 0
 timeCnt = 0
 start = time.time()
-
+dataArr = []
 while timeCnt < TIMESET:
     data = ser.read(PACKET_SIZE)
-        
+
+    
+
+    dataArr.append(data)
+
     if data[0] == 0xaa and data[1] == 0x01:
         count += 1
 
@@ -20,3 +25,11 @@ while timeCnt < TIMESET:
         print(f"{count} packets/s")
         count = 0
         start = time.time()
+        
+filename = datetime.now().strftime("pressData_%Y%m%d_%H%M%S.txt")
+
+with open(filename, "w", encoding="utf-8") as f:
+    for data in dataArr:
+        f.write(data.hex(" ") + "\n")
+
+print(f"Saved to {filename}")
